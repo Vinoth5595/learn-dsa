6a470f0113f5f9671aa7f99b4ad6cd199a9d18d6
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-10-17 13:50:03 +0530

    Reverse a String

---
+++ src/learn/ds/string/ReverseAString.java
@@ -0,0 +1,23 @@
+package learn.ds.string;
+
+/**
+ * Reverse a String
+ * 
+ * @author vinoth
+ *
+ */
+public class ReverseAString {
+
+	public static void main(String[] args) {
+		String myName = "Vinoth";
+		String reversedString = "";
+		char[] nameCharArr = myName.toCharArray();
+
+		for (int i = nameCharArr.length - 1; i >= 0; i--) {
+			reversedString += nameCharArr[i];
+		}
+
+		System.out.println(reversedString);
+	}
+
+}
