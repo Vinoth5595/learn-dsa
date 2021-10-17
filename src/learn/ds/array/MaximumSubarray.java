16cbcafa540904232a728b4b533df111ee41a53b
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-10-17 22:44:36 +0530

    Added Kadane's Algorithm implementation

--- src/learn/ds/array/MaximumSubarray.java
+++ src/learn/ds/array/MaximumSubarray.java
@@ -5,8 +5,42 @@ public class MaximumSubarray {
 	public static void main(String[] args) {
 		int[] intArray = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 
-		// Brute force method - O(n)
+		// Brute force method - O(n^2)
 		findMaximumSubarray(intArray);
+		// Kadane's algorithm - O(n)
+		findMaxSubArrayUsingKadane(intArray);
+		
+	}
+	
+	/**
+	 * Find maximum sub array using Kadane's Algorithm
+	 * 
+	 * @param intArray
+	 */
+	private static void findMaxSubArrayUsingKadane(int[] intArray) {
+		int max_so_far = Integer.MIN_VALUE;
+		int max_ending_here = 0;
+		int start = 0;
+		int end = 0;
+		int s = 0;
+		
+		for(int i = 0; i < intArray.length; i++) {
+			max_ending_here += intArray[i];
+			
+			if(max_so_far < max_ending_here) {
+				max_so_far = max_ending_here;
+				start = s;
+				end = i;
+			}
+			
+			if(max_ending_here < 0) {
+				max_ending_here = 0;
+				s = i + 1;
+			}
+			
+		}
+		
+		System.out.println("Start=" + start + ", end=" + end + ", Sum=" + max_so_far);
 	}
 
 	/**
