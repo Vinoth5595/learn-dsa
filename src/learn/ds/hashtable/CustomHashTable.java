33af46d65db461440bcd32460dd65932fa938115
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-10-22 23:01:23 +0530

    Template for Custom Hash Table

---
+++ src/learn/ds/hashtable/CustomHashTable.java
@@ -0,0 +1,46 @@
+package learn.ds.hashtable;
+
+class Node<K, V>{
+	int hash;
+	K key;
+	V value;
+	Node<K, V> next;
+	
+	Node(int hash, K key, V value, Node<K, V> next){
+		this.hash = hash;
+		this.key = key;
+		this.value = value;
+		this.next = next;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+	
+	
+	
+	
+}
+
+public class CustomHashTable {
+
+	public static void main(String[] args) {
+		int a  = -1454;
+		
+		System.out.println(~5);
+	}
+
+}
