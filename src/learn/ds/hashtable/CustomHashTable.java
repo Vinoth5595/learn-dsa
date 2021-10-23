91eaf0a7f2df928930eb699a3308e802604d41a2
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-10-23 23:28:28 +0530

    HashTable Base Code

--- src/learn/ds/hashtable/CustomHashTable.java
+++ src/learn/ds/hashtable/CustomHashTable.java
@@ -1,54 +1,71 @@
 package learn.ds.hashtable;
 
-/**
- * Node class
- * 
- * @author vinoth
- *
- * @param <K> the key
- * @param <V>  the value
- */
-class Node<K, V>{
-	int hash;
+import java.util.ArrayList;
+import java.util.Objects;
+
+class HashNode<K, V>{
 	K key;
 	V value;
-	Node<K, V> next;
+	final int hashCode;
+	HashNode<K, V> next;
 	
-	Node(int hash, K key, V value, Node<K, V> next){
-		this.hash = hash;
+	HashNode(K key, V value, final int hashCode){
 		this.key = key;
 		this.value = value;
-		this.next = next;
-	}
-
-	public K getKey() {
-		return key;
-	}
-
-	public void setKey(K key) {
-		this.key = key;
-	}
-
-	public V getValue() {
-		return value;
-	}
-
-	public void setValue(V value) {
-		this.value = value;
+		this.hashCode = hashCode;
 	}
 	
+	@Override
+	public String toString() {
+		return key + "=" + value;
+	}
+}
+
+class HashMap<K, V>{
+	private ArrayList<HashNode<K, V>> bucketArray;
 	
+	int bucketCapacity;
 	
+	int size;
 	
+	HashMap(){
+		bucketArray = new ArrayList<>();
+		bucketCapacity = 10;
+		size = 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int hashCode(K key) {
+		return Objects.hash(key);
+	}
+	
+	public int getBucketIndex(K key) {
+		int hashCode = hashCode(key);
+		int bucketIndex = hashCode % bucketCapacity;
+		return bucketIndex < 0 ? bucketIndex * -1 : bucketIndex;
+	}
+	
+//	public V remove(K key) {
+//		int bucketIndex = getBucketIndex(key);
+//		int hashCode = hashCode(key);
+//		
+//		HashNode<K, V> head = bucketArray.get(bucketIndex);
+//		
+//		if(head != null) {
+//			bucketArray.set(bucketIndex, null);
+//			size--;
+//		}
+//		
+//	}
 	
 }
 
 public class CustomHashTable {
 
 	public static void main(String[] args) {
-		int a  = -1454;
-		
-		System.out.println(~5);
 	}
 
 }
