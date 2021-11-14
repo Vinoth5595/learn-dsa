0e19c33eaf0f0720f3e66249e7e445e3389a829d
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-11-14 12:22:56 +0530

    method to swap numbers

--- src/learn/coding/Common.java
+++ src/learn/coding/Common.java
@@ -1,25 +1,34 @@
 package learn.coding;
 
-class Code{
-	
+class Code {
+
 	/**
 	 * To reverse a String
 	 * 
 	 * @param value - input value
 	 */
 	public void reverseAString(String value) {
-		if(value == null || value.trim().equals("")) {
+		if (value == null || value.trim().equals("")) {
 			throw new IllegalArgumentException("Invalid Argument");
 		}
-		
+
 		char[] charArr = value.toCharArray();
 		StringBuilder stringBuilder = new StringBuilder();
-		
-		for(int i = charArr.length - 1; i >= 0; i--) {
+
+		for (int i = charArr.length - 1; i >= 0; i--) {
 			stringBuilder.append(charArr[i]);
 		}
-		
+
 		System.out.println(stringBuilder); // Prints reversed string
+	}
+
+	public void swapTwoNumbers(int a, int b) {
+		System.out.printf("a is %s, b is %s :: ", a, b);
+		b = a + b; // if a = 1, b = 2, a + b = 3
+		a = b - a; // 3 - 1 = 2
+		b = b - a; // 3 - 2 = 1
+
+		System.out.printf("After swapping: a is %s, b is %s", a, b);
 	}
 }
 
@@ -28,5 +37,6 @@ public class Common {
 	public static void main(String[] args) {
 		Code code = new Code();
 		code.reverseAString("Malayalam"); // To reverse a String
+		code.swapTwoNumbers(1, 2);
 	}
 }
