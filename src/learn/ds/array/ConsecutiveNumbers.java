941b37854ae35673cb7b99b9cf7ef98909857c92
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-11-26 00:10:21 +0530

    Approach 2 - get consecutive array

--- src/learn/ds/array/ConsecutiveNumbers.java
+++ src/learn/ds/array/ConsecutiveNumbers.java
@@ -1,10 +1,40 @@
 package learn.ds.array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConsecutiveNumbers {
 
 	public static void main(String[] args) {
 		int[] arr = { 6, 1, 9, 3, 10, 4, 20, 2, 5 };
 		System.out.println(findMaximumConsecutiveNumber(arr));
+		System.out.println(findMaximumConsecutiveNumber2(arr));
+	}
+
+	private static int findMaximumConsecutiveNumber2(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+		
+		for(int value: arr) {
+			set.add(value);
+		}
+		
+		int longestStreak = 0;
+		
+		for(int num: set) {
+			if(!set.contains(num-1)) {
+				int currentNum = num;
+				int currentStreak = 1;
+				
+				while(set.contains(currentNum + 1)) {
+					currentNum += 1;
+					currentStreak += 1;
+				}
+				
+				longestStreak = Math.max(currentStreak, longestStreak);
+			}
+		}
+		
+		return longestStreak;
 	}
 
 	private static int findMaximumConsecutiveNumber(int[] arr) {
