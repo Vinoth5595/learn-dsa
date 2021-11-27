5ea70572dfb08cc9c6d42a3dc62d1909a144b67d
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-11-27 23:48:21 +0530

    binary search iterative approach

--- src/learn/ds/array/BinarySearch.java
+++ src/learn/ds/array/BinarySearch.java
@@ -7,12 +7,37 @@ public class BinarySearch {
 		int searchValue = 8;
 
 		int binarySearch = binarySearch(intArr, 0, intArr.length - 1, searchValue);
+		if (binarySearch == -1) {
+			System.out.println("Value not found!!!");
+		} else {
+			System.out.println(binarySearch + " - Found!!!");
+		}
+		
+		binarySearch = binarySearch2(intArr, 0, intArr.length - 1, searchValue);
 
 		if (binarySearch == -1) {
 			System.out.println("Value not found!!!");
 		} else {
 			System.out.println(binarySearch + " - Found!!!");
 		}
+	}
+
+	private static int binarySearch2(int[] intContainer, int left, int right, int valueToFind) {
+		while(right >= left) {
+			int mid = (left + right) / 2;
+			
+			if(intContainer[mid] == valueToFind) {
+				return intContainer[mid];
+			}
+			
+			if(intContainer[mid] < valueToFind) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		
+		return -1;
 	}
 
 	private static int binarySearch(int[] intArr, int left, int right, int searchValue) {
