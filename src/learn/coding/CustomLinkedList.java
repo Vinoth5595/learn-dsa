2511c929edac5ea3ad187df7b0e9d7379319709e
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-12-21 00:57:58 +0530

    Linked list basic structure

---
+++ src/learn/coding/CustomLinkedList.java
@@ -0,0 +1,24 @@
+package learn.coding;
+
+class LList {
+	Node head;
+
+	static class Node {
+		Node next;
+		int data;
+
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+}
+
+public class CustomLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
