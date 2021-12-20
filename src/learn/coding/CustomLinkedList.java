8f88320a6176cdda445c65f6fec74b6ca0b7998c
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-12-21 01:04:49 +0530

    Logic to add node at the end of the list

--- src/learn/coding/CustomLinkedList.java
+++ src/learn/coding/CustomLinkedList.java
@@ -12,6 +12,34 @@ class LList {
 			this.next = null;
 		}
 	}
+	
+	// Method to insert new node at the end
+	public static LList insert(LList linkedList, int data) {
+		/*
+		 * Create new node.
+		 * if list is empty points it to head node.
+		 * Else traverse till we find the last node.
+		 * Then link the node at the last node
+		 * 
+		 */
+		
+		Node newNode = new Node(data);
+		newNode.next = null;
+		
+		if(linkedList.head == null) {
+			linkedList.head = newNode;
+		} else {
+			Node lastNode = linkedList.head;
+			
+			while(lastNode.next != null) {
+				lastNode = lastNode.next;
+			}
+			
+			lastNode = newNode;
+		}
+		
+		return linkedList;
+	}
 }
 
 public class CustomLinkedList {
