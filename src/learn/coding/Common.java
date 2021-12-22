7bd8127576f13ef6a286af3e5c893f5e93fd5584
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-12-23 00:28:36 +0530

    Sort map by key

--- src/learn/coding/Common.java
+++ src/learn/coding/Common.java
@@ -1,70 +1,78 @@
 package learn.coding;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Player implements Comparable<Player> {
 	private String name;
 	private Integer age;
 	private Integer rank;
-	
+
 	public Player(String name, Integer age, Integer rank) {
 		super();
 		this.name = name;
 		this.age = age;
 		this.rank = rank;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
 	public Integer getRank() {
 		return rank;
 	}
+
 	public void setRank(Integer rank) {
 		this.rank = rank;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "Player [name=" + name + ", age=" + age + ", rank=" + rank + "]";
 	}
+
 	@Override
 	public int compareTo(Player o) {
 		return Integer.compare(this.getRank(), o.getRank());
 	}
 }
 
-class AgeComparator implements Comparator<Player>{
+class AgeComparator implements Comparator<Player> {
 
 	@Override
 	public int compare(Player o1, Player o2) {
 		return Integer.compare(o1.getAge(), o2.getAge());
 	}
-	
+
 }
 
-class NameComparator implements Comparator<Player>{
+class NameComparator implements Comparator<Player> {
 
 	@Override
 	public int compare(Player o1, Player o2) {
 		return o1.getName().compareTo(o2.getName());
 	}
-	
-}
 
+}
 
 class Code {
 
@@ -181,58 +189,58 @@ class Code {
 
 		System.out.println(stringBuilder.toString().equals(userInput) ? "Palindrome" : "Not Palindrome");
 	}
-	
+
 	public void removeWhiteSpaces(String userInput) {
-		if(userInput == null || userInput.strip().equals("")) {
+		if (userInput == null || userInput.strip().equals("")) {
 			throw new IllegalArgumentException("invalid input");
 		}
-		
+
 		StringBuilder stringBuilder = new StringBuilder();
 		char[] charArr = userInput.toCharArray();
-		
-		for(char c: charArr) {
-			if(!Character.isWhitespace(c)) {
+
+		for (char c : charArr) {
+			if (!Character.isWhitespace(c)) {
 				stringBuilder.append(c);
 			}
 		}
-		
+
 		System.out.println(stringBuilder);
 	}
-	
+
 	public void sortArray(int[] intArr) {
 		Arrays.sort(intArr);
-		
+
 		System.out.println(Arrays.toString(intArr));
 	}
-	
+
 	private int factorial(int num) {
-		if(num == 1) return 1;
+		if (num == 1)
+			return 1;
 		return num * factorial(num - 1);
 	}
-	
+
 	public void factorialNumber(int n) {
 		System.out.println(factorial(n));
 	}
-	
+
 	public void pyramidPattern(int n) {
-		for(int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			int whiteSpaces = n - i;
-			
+
 			printString(" ", whiteSpaces);
-			
-			printString( i + " ", i);
-			
+
+			printString(i + " ", i);
+
 			System.out.println();
 		}
 	}
 
 	private void printString(String string, int n) {
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			System.out.print(string);
 		}
 	}
 }
-
 
 public class Common {
 
@@ -251,7 +259,7 @@ public class Common {
 //		code.sortArray(new int[] {4, 6, 1, 2});
 //		code.factorialNumber(5);
 //		code.pyramidPattern(5);
-		
+
 //		Player player1 = new Player("Vinoth", 26, 1);
 //		Player player2 = new Player("Nirmal", 10, 3);
 //		Player player3 = new Player("Jaya", 30, 2);
@@ -264,33 +272,56 @@ public class Common {
 //		System.out.println("Based on Age:" + playerList);
 //		Collections.sort(playerList, new NameComparator());
 //		System.out.println("Based on Name:" + playerList);
-		
-		LinkedList<Integer> linkedList = new LinkedList<>();
-		linkedList.add(1);
-		linkedList.add(2);
-		linkedList.add(3);
-		reverseLinkedList(linkedList);
-		
-		int[] arr = {1,5,7,8,1,9,43,2};
-		bubbleSort(arr);
+
+//		LinkedList<Integer> linkedList = new LinkedList<>();
+//		linkedList.add(1);
+//		linkedList.add(2);
+//		linkedList.add(3);
+//		reverseLinkedList(linkedList);
+//		
+//		int[] arr = {1,5,7,8,1,9,43,2};
+//		bubbleSort(arr);
+
+		Map<String, Integer> unsortMap = new HashMap<>();
+		unsortMap.put("z", 10);
+		unsortMap.put("b", 5);
+		unsortMap.put("a", 6);
+		unsortMap.put("c", 20);
+		unsortMap.put("d", 1);
+		unsortMap.put("e", 7);
+		unsortMap.put("y", 8);
+		unsortMap.put("n", 99);
+		unsortMap.put("g", 50);
+		unsortMap.put("m", 2);
+		unsortMap.put("f", 9);
+
+		sortMapbyKeyjava8(unsortMap);
+	}
+
+	private static void sortMapbyKeyjava8(Map<String, Integer> unsortMap) {
+		LinkedHashMap<String, Integer> collect = unsortMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+
+		System.out.println(collect);
 	}
 
 	private static void bubbleSort(int[] arr) {
 		boolean swapped;
-		for(int i = 0; i < arr.length - 1; i++) {
+		for (int i = 0; i < arr.length - 1; i++) {
 			swapped = false;
-			for(int j = 0; j < arr.length - i - 1; j++) {
-				if(arr[j] > arr[j + 1]) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
 					swapped = true;
 				}
 			}
-			
-			if(swapped == false) break;
+
+			if (swapped == false)
+				break;
 		}
-		
+
 		System.out.println(Arrays.toString(arr));
 	}
 
