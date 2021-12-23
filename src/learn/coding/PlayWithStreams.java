8c4b035fcce0a9e6d2f97738fb8df2a46a4392a9
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2021-12-23 18:41:20 +0530

    Playing with Streams

--- src/learn/coding/PlayWithStreams.java
+++ src/learn/coding/PlayWithStreams.java
@@ -1,8 +1,16 @@
 package learn.coding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
 
 class ThreadRunner {
 	public static void runThread(final int delay) {
@@ -18,9 +26,79 @@ class ThreadRunner {
 public class PlayWithStreams {
 
 	public static void main(String[] args) {
-		playWithThread();
+		//playWithThread();
+		
+		// Create Streams
+		
+		// Fixed integers
+		Stream<String> stream = Stream.of("Hello", "World", "Buddies");
+		stream.forEach(System.out::println);
+		
+		// Arrays
+		Stream<String> stream2 = Stream.of(new String[]{"Hello", "World", "Buddies"});
+		stream2.forEach(System.out::println);
+		
+		// Lists
+		Stream<Integer> stream3 = Arrays.asList(1,2,3).stream();
+		stream3.forEach(System.out::println);
+		
+		// Stream Generate
+		Stream<Integer> generateRandom = Stream.generate(() -> (new Random()).nextInt(100));
+		generateRandom.limit(5).forEach(System.out::println);
+		
+		// Stream.iterate
+		Stream.iterate(1, n -> n + 1).limit(20).forEach(System.out::println);
+		
+		// Stream.iterate fibonacci series
+		Stream.iterate(new int[] {0, 1}, n -> new int[] {n[1], n[0] + n[1]})
+		.limit(20)
+		.map(n -> n[0])
+		.forEach(System.out::println);
+		
+		
+		System.out.println("*************************");
+		
+		
+		
+		IntStream intStream = "12345_abcdefg".chars();
+		intStream.forEach(System.out::println);
+		
+		Stream<String> streamStr = Stream.of("A$B$C".split("\\$"));
+		streamStr.forEach(p -> System.out.println(p));
+		
+		
+		Builder<String> stringBuilder = Stream.builder();
+		Stream<String> build = stringBuilder.add("This").add("is").add("stream").add("builder").build();
+		build.forEach(System.out::println);
+		
+		List<Integer> collect = Stream.iterate(0, n -> ++n)
+		.limit(10)
+		.collect(Collectors.toList());
+		
+		System.out.println(collect);
+		
+		Stream.of("India", "Australia", "Newzealand", "", "South Africa", "England")
+	      .takeWhile(o-> !o.startsWith("N"))
+	      .forEach(System.out::println);
+		
+		Stream.of("", "Australia", "", "England", "Srilanka", "abc")
+	      .dropWhile(o-> !o.startsWith("S"))
+	      .forEach(System.out::println);
+		
+		List<String> memberNames = new ArrayList<>();
+		memberNames.add("Amitabh");
+		memberNames.add("Shekhar");
+		memberNames.add("Aman");
+		memberNames.add("Rahul");
+		memberNames.add("Shahrukh");
+		memberNames.add("Salman");
+		memberNames.add("Yana");
+		memberNames.add("Lokesh");
+		
+		memberNames.stream().sorted(Collections.reverseOrder()).forEach(System.out::println);
+		
 	}
-
+	
 	private static void playWithThread() {
 		ExecutorService es = Executors.newFixedThreadPool(5);
 		
@@ -33,6 +111,7 @@ public class PlayWithStreams {
 		long t1 = System.currentTimeMillis();
 		
 		Stream.of(d1, d2, d3, d4, d5)
+		.parallel()
 		.forEach(val -> {
 			es.execute(() -> ThreadRunner.runThread(val));
 		});
@@ -44,4 +123,11 @@ public class PlayWithStreams {
 		es.shutdown();
 	}
 
+}
+
+class TestThread extends Thread{
+	@Override
+	public void run() {
+		System.out.println("Test");
+	}
 }
