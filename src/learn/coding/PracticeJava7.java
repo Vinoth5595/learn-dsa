74a46466c928d687a04f2af66fd1d86c2ed72259
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-01 12:28:05 +0530

    Strings in switch's Selector

--- src/learn/coding/PracticeJava7.java
+++ src/learn/coding/PracticeJava7.java
@@ -3,6 +3,16 @@ package learn.coding;
 public class PracticeJava7 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String day = "SAT";
+        switch (day) {   // switch on String selector
+            case "MON": System.out.println("Monday"); break;
+            case "TUE": System.out.println("Tuesday"); break;
+            case "WED": System.out.println("Wednesday"); break;
+            case "THU": System.out.println("Thursday"); break;
+            case "FRI": System.out.println("Friday"); break;
+            case "SAT": System.out.println("Saturday"); break;
+            case "SUN": System.out.println("Sunday"); break;
+            default: System.out.println("Invalid");
+        }
     }
 }
