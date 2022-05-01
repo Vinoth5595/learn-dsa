a9165901636f9b11ca6d8be0d5c54c10a262a2d2
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-01 12:32:48 +0530

    Binary Integer Literals with Prefix "0b" and Underscore in Numeric Literals

--- src/learn/coding/PracticeJava7.java
+++ src/learn/coding/PracticeJava7.java
@@ -14,5 +14,24 @@ public class PracticeJava7 {
             case "SUN": System.out.println("Sunday"); break;
             default: System.out.println("Invalid");
         }
+
+        // Some 32-bit 'int' literal values
+        int anInt1 = 0b0101_0000_1010_0010_1101_0000_1010_0010;
+        int anInt2 = 0b0011_1000;
+
+        // An 8-bit 'byte' literal value. By default, literal values are 'int'.
+        // Need to cast to 'byte'
+        byte aByte = (byte)0b0110_1101;
+
+        // A 16-bit 'short' literal value
+        short aShort = (short)0b0111_0101_0000_0101;
+
+        // A 64-bit 'long' literal value. Long literals requires suffix "L".
+        long aLong = 0b1000_0101_0001_0110_1000_0101_0000_1010_0010_1101_0100_0101_1010_0001_0100_0101L;
+
+        // Formatted output: "%d" for integer in decimal, "%x" in hexadecimal, "%o" in octal.
+        // Take note that "%b" prints true or false (for null), NOT binary.
+        System.out.printf("%d(%x)(%o)(%b)\n", anInt1, anInt1, anInt1, anInt1);
+        System.out.printf("%d(%x)(%o)(%b)\n", aByte, aByte, aByte, aByte);
     }
 }
