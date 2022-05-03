a739c8cdb71c390a165332b470b19c1c82ffb11e
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-03 11:02:35 +0530

    class template

---
+++ src/learn/coding/PracticeJava8.java
@@ -0,0 +1,12 @@
+package learn.coding;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PracticeJava8 {
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO, "Hello World");
+    }
+}
