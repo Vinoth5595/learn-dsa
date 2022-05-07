096a94dcdfac22c8e995151e41e64e950e63bfa4
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-07 21:10:01 +0530

    Java 10 - Local Variable Type inference

--- src/learn/coding/PracticeJava10.java
+++ src/learn/coding/PracticeJava10.java
@@ -1,5 +1,6 @@
 package learn.coding;
 
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,10 +8,23 @@ public class PracticeJava10 {
     public static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public static void checkVersion(){
-        logger.log(Level.INFO,"Version : " + Runtime.version());
+        logger.log(Level.INFO,"Version : {0}", Runtime.version());
+    }
+
+    public static void localVariableTypeInference(){
+        var numbers = Arrays.asList(1,2,3);
+
+        for(var num: numbers){
+            logger.log(Level.INFO,  "{0}",  num);
+        }
+
+        for(var i = 0; i < numbers.size(); i++){
+            logger.log(Level.INFO,  "{0}", numbers.get(i));
+        }
     }
 
     public static void main(String[] args) {
         checkVersion();
+        localVariableTypeInference();
     }
 }
