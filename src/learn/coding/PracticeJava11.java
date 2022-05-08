d8022abe909dbc5a16e00e2005be1ef1dcba5897
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-08 13:19:12 +0530

    Java 11 - local variable syntax for lambda expression

--- src/learn/coding/PracticeJava11.java
+++ src/learn/coding/PracticeJava11.java
@@ -3,9 +3,16 @@ package learn.coding;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+interface Addition<T> {
+    T add(T val1, T val2);
+}
+
 public class PracticeJava11 {
     public static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
+    /**
+     * Java 11 - String operations
+     */
     public static void stringOperations(){
         // isBlank
         logger.log(Level.INFO, "{0}", " ".isBlank());
@@ -26,7 +33,16 @@ public class PracticeJava11 {
         logger.log(Level.INFO, () -> val1.repeat(10));
     }
 
+    /**
+     * Local variable syntax for lambda expression
+     */
+    public static void localVariableVar(){
+        Addition<Integer> addition = (var a, var b) -> a + b;
+        logger.log(Level.INFO, () -> addition.add(1,2)+"");
+    }
+
     public static void main(String[] args) {
         stringOperations();
+        localVariableVar();
     }
 }
