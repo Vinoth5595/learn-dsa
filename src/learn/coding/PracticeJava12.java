8e3ff06e989ec5f872b87d710081e3c9d607f344
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-13 21:13:42 +0530

    Java 12 - New methods in String

--- src/learn/coding/PracticeJava12.java
+++ src/learn/coding/PracticeJava12.java
@@ -1,10 +1,14 @@
 package learn.coding;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -80,6 +84,9 @@ public class PracticeJava12 {
         logger.info(() -> numberFormat.format(2346));
     }
 
+    /**
+     * Collectors Teeing method
+     */
     public static void collectorsTeeing() {
         double mean = Stream.of(1, 2, 3, 4, 5)
                 .collect(Collectors.teeing(summingDouble(i -> i),
@@ -87,6 +94,30 @@ public class PracticeJava12 {
                         (sum, n) -> sum / n));
 
         logger.info(() -> mean + "");
+    }
+
+    /**
+     * String New Methods
+     */
+    public static void stringNewMethods(){
+        String data = "Vinoth";
+
+        // indent
+        logger.info(() -> data.indent(10));
+
+        // transform
+        String val = "One,two,three";
+        List<String> strList = val.transform(s -> Arrays.asList(val.split(",")));
+        logger.info(strList + "");
+
+        // describeConstable
+        String val1 = "test";
+        Optional<String> opt = val1.describeConstable();
+        logger.info(() -> opt.get());
+
+        // resolveConstantDesc
+        String val2 = "test abc";
+        logger.info(() -> val2.resolveConstantDesc(MethodHandles.lookup()));
     }
 
     /**
@@ -99,5 +130,6 @@ public class PracticeJava12 {
         filesMismatch();
         compactNumberFormatting();
         collectorsTeeing();
+        stringNewMethods();
     }
 }
