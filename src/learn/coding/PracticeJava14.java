445706e0fc20480987451352283f3c5a98b3be0d
Vinoth Manoharan <vinothm.manoharan@gmail.com>
2022-05-14 14:32:33 +0530

    Java 14 - Helpful NPE and Record in Java

---
+++ src/learn/coding/PracticeJava14.java
@@ -0,0 +1,30 @@
+package learn.coding;
+
+import java.util.logging.Logger;
+
+record Author(Integer id, String name){}
+
+public class PracticeJava14 {
+    public static final Logger logger = Logger.getLogger(PracticeJava14.class.getName());
+
+    /**
+     * Helpful NullPointerExceptions
+     */
+    public static void helpfulNPE(){
+        Object a = null;
+        //a.hashCode(); // Commented to avoid NPE
+     }
+
+    /**
+     * Record in Java
+     */
+    public static void recordTesting(){
+        Author author = new Author(1, "Vinoth");
+        logger.info(() -> author.name());
+    }
+
+    public static void main(String[] args) {
+        helpfulNPE();
+        recordTesting();
+    }
+}
